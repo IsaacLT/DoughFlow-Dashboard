1c42d142c654d53741483a37175453ed0e4f00c3
Isaac Lindegren Ternbom <isaacl@student.chalmers.se>
2023-09-08 13:23:26 +0200

    #3 Create schema model for budget entity
    Create models and controller folder in server directory. Create budget.js file in models folder for budget schema.

---
+++ server/models/budget.js
@@ -0,0 +1,37 @@
+// Import mongoose
+const mongoose = require('mongoose');
+
+// Connect to MongoDB
+var db = mongoose.connect( 'mongodb://0.0.0.0:27017/', {useNewUrlParser: true});
+
+// Create schema for budget entity
+var budget = new mongoose.Schema({
+
+    // Budget name
+    name: {
+        type: String, 
+        required: true
+    }, 
+    // Budget amount
+    amount: {
+        type: Number,
+        required: true
+    },
+    // Reference to the user model
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'user'
+    },
+    // Reference to the expense model
+    expenses: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'expense' 
+    }],
+    //Reference to the catergory model
+    categories: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'category'
+    }],
+});
+
+module.exports = mongoose.model('budget', budget);
