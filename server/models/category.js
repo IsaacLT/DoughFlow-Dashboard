a59c368e3c6cffe98e202dbba61e88475b66f9e0
Karl Eriksson <kaeriks@student.chalmers.se>
2023-09-13 16:07:27 +0200

    add category schema for category entity

---
+++ server/models/category.js
@@ -0,0 +1,19 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+var mongoURI = process.env.MONGODB_URI || 'mongodb://0.0.0.0:27017/';
+var db = mongoose.connect(mongoURI, {useNewUrlParser: true});
+
+
+var categorySchema = new Schema({
+    categoryName : {
+        type : String,
+        required : true
+    }, 
+    expenses : [{
+        type: Schema.Types.ObjectId,
+        ref: "expense" 
+    }]
+});
+
+var Category = mongoose.model('category', categorySchema);
+module.exports = Category;
