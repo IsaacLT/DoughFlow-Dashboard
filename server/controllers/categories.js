6ad1b6c6b3f331957799a15db3213d42b79f129b
Karl Eriksson <kaeriks@student.chalmers.se>
2023-09-14 17:55:36 +0200

    Add operations for expenses and category relationship and tests for these

--- server/controllers/categories.js
+++ server/controllers/categories.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const Category = require('../models/category');
+const Expense = require('../models/expense');
 
 router.post('/categories', async function (req, res) {
     const name = req.body.categoryName;
@@ -58,5 +59,74 @@ router.delete('/categories/:id', async function(req, res) {
     await Category.deleteOne(category);
     res.json({message: 'Category deleted succesfully'});
 });
+
+router.post('/categories/:id/expenses', async function (req, res) {
+    const categoryId = req.params.id;
+        // Find the category by Id
+    const category = await Category.findById(categoryId);
+
+    if (!category) {
+        return res.status(404).json({ message: 'Category not found' });
+        }
+        // Create a new expense based on the request body
+        const newExpense = new Expense({
+            description: req.body.description,
+            amount: req.body.amount,
+            userId: '6503106b83a74e0faa10c19d', //random ID for testing purposes
+            date: req.body.date,
+            categoryId: category._id
+        });
+        // Save the new expense
+        await newExpense.save();
+        // Add the new expense to the user's expenses array
+        category.expenses.push(newExpense);
+        await category.save();
+        res.json(newExpense);
+});
+
+router.get('/categories/:id/expenses', async function (req, res) {
+    const categoryId = req.params.id;
+        // Query the database to retrieve a user by ID
+        const category = await Category.findById(categoryId).populate("expenses");
+        if (!category) {
+            return res.status(404).json({ message: 'Category not found' });
+        }
+        // Send the retrieved user data as the response
+        res.json(category);
+});
+
+router.get('/categories/:id/expenses/:expenseid', async function (req, res) {
+    const categoryId = req.params.id;
+    const expenseId = req.params.expenseid;
+    // Find the category by Id
+    const category = await Category.findById(categoryId);
+    if (!category) {
+        return res.status(404).json({ message: 'Category not found' });
+    }
+    // Find the expense by its ID and ensure it belongs to the category
+    const expense = await Expense.findOne({ _id: expenseId, categoryId: category._id });
+    if (!expense) {
+        return res.status(404).json({ message: 'Expense not found', expenseId, category});
+    }
+    res.json(expense);
+});
+
+router.delete('/categories/:id/expenses/:expenseid', async function (req, res) {
+    const categoryId = req.params.id;
+    const expenseId = req.params.expenseid;
+    // Find the category by Id
+    const category = await Category.findById(categoryId);
+    if (!category) {
+        return res.status(404).json({ message: 'Category not found' });
+    }
+    // Find the expense by its ID and ensure it belongs to the user
+    const expense = await Expense.findOne({ _id: expenseId, categoryId: category._id });
+    if (!expense) {
+        return res.status(404).json({ message: 'Expense not found', categoryId, expenseId});
+    }
+    await Expense.deleteOne(expense);
+    res.json({message: 'Expense deleted succesfully', expense});
+});
+
 
 module.exports = router;
--- server/models/category.js
+++ server/models/category.js
@@ -11,7 +11,7 @@ var categorySchema = new Schema({
     }, 
     expenses : [{
         type: Schema.Types.ObjectId,
-        ref: "expense" 
+        ref: "expenseModel" 
     }]
 });
 
