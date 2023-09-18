92b613307b13a5e7adf521d81baa7f9fa4617a56
Isaac Lindegren Ternbom <isaacl@student.chalmers.se>
2023-09-18 10:09:53 +0200

    #9 Add CRUD operations for budget relationships

--- server/controllers/budgets.js
+++ server/controllers/budgets.js
@@ -2,6 +2,7 @@
 const express = require('express');
 const router = express.Router();
 const Budget = require('../models/budget');
+const Category = require('../models/category');
 
 // Create a new budget instance
 router.post('/budgets', async (req,res) => {                              
@@ -17,16 +18,16 @@ router.post('/budgets', async (req,res) => {
 // Get all budgets
 router.get('/budgets', async (req,res) => {
     const budgets = await Budget.find({});        // Query the database to get all budgets
-    if (budgets.length == 0) {                             // If no budgets exist, respond with object not found error
+    if (budgets.length == 0) {                    // If no budgets exist, respond with object not found error
         return res.status(404).json({error: 'No budgets exist.'})
     } else {
-    res.json(budgets);                          // Send the retrieved budgets as the respose
+    res.json(budgets);                            // Send the retrieved budgets as the respose
     }
 });
 
 // Get a budget instance
 router.get('/budgets/:id', async (req, res) => {
-    const budget = await Budget.findById(req.params.id);      // Request id from URL parameters
+    const budget = await Budget.findById(req.params.id);    // Request id from URL parameters
     if (!budget) {                                          // If no budget exists, respond with object not found error
         return res.status(404).json({ error: 'Budget not found'});
     } else {
@@ -69,7 +70,7 @@ router.patch('/budgets/:id', async (req, res) => {
 router.delete('/budgets', async (req, res) => {
     const result = await Budget.deleteMany({});       // {} matches all objects in the Budget collection
     if (!result.deletedCount) {
-        return res.status(404).json({ error: 'No budgets found'});
+        return res.status(404).json({ error: 'Budget not found'});
     } else {
         res.json({ message: `Deleted ${result.deletedCount} budgets`});
     }
@@ -90,7 +91,7 @@ router.post('/budgets/:id/categories', async (req, res) => {
     const budgetId = req.params.id;
     const budget = await budget.findById(budgetId);     // Find the category by Id
     if (!budget) {
-         return res.status(404).json({ message: 'Budget not found' });
+         return res.status(404).json({ error: 'Budget not found' });
     } 
     const newCategory = new Category(req.body);         // Create a new category based on the request body
     await newCategory.save();                           // Save the new category
@@ -98,6 +99,51 @@ router.post('/budgets/:id/categories', async (req, res) => {
     await budget.save();                                // Save budget
     res.json(newCategory);
 });
+
+// Get operation for endpoint budget/:id/categories
+router.get('budgets/:id/categories', async (req, res) => {
+    const budgetId = req.params.id;
+    const budget = await Budget.findById(budgetId).populate("categories");       // Query the database to retrieve a budget by ID
+    if (!budget) {
+        return res.status(404).json({ error: 'Budget not found' });
+    }
+    // Send the retrieved user data as the response
+    res.json(budget);
+});
+
+// Get operation for endpoint budget/:id/categories/:id
+router.get('budgets/:id/categories/:categoryid', async (req, res) => {
+    const budgetId = req.params.id;
+    const categoryId = req.params.categoryid;
+    const budget = await Budget.findById(budgetId);
+    if (!budget) {
+        return res.status(404).json({ error: 'Budget not found' });
+    }
+    // Find the category by its ID and ensure it belongs to the budget
+    const category = await Category.findOne({ _id: categoryId, budgetId: budget._id });
+    if (!category) {
+        return res.status(404).json({ error: 'Category not found', categoryId, budget});
+    }
+    res.json(category);
+});
+
+// Delete operation for endpoint budget/:id/categories/:id
+router.delete('/budgets/:id/categories/:categoryid', async (req, res) => {
+    const budgetId = req.params.id;
+    const categoryId = req.params.expenseid;
+    const budget = await Budget.findById(budgetId);
+    if (!budget) {
+        return res.status(404).json({ error: 'Budget not found' });
+    }
+    // Find the category by its ID and ensure it belongs to the budget
+    const category = await Category.findOne({ _id: categoryId, budgetId: budget._id });
+    if (!category) {
+        return res.status(404).json({ error: 'Category not found', categoryId, budget});
+    }
+    await Category.deleteOne(category);
+    res.json({message: 'Category deleted succesfully', category});
+});
+
 
 
 module.exports = router;
