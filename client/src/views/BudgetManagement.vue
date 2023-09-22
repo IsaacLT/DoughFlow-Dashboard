44cf1ed19a0d744d7fe83a2df83324728dc5b4e8
Malte Bengtsson <bmalte@student.chalmers.se>
2023-09-22 10:13:33 +0200

    Add router file. Create Vue file for BudgetManagement

---
+++ client/src/views/BudgetManagement.vue
@@ -0,0 +1,53 @@
+<template>
+    <div class="container">
+      <!-- Header -->
+      <header>
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+          <a class="navbar-brand" href="#">Budget Management</a>
+          <!-- Add navigation links or other header content here -->
+        </nav>
+      </header>
+  
+      <!-- Main Content -->
+      <main>
+        <!-- Page Title -->
+        <h1>Budget Overview</h1>
+  
+        <!-- Budget Summary -->
+        <div class="card">
+          <div class="card-header">
+            Budget Summary
+          </div>
+          <div class="card-body">
+            <!-- Add content for budget summary here -->
+          </div>
+        </div>
+  
+        <!-- Expense List -->
+        <div class="card mt-3">
+          <div class="card-header">
+            Expense List
+          </div>
+          <div class="card-body">
+            <!-- Add content for expense list here -->
+          </div>
+        </div>
+      </main>
+  
+      <!-- Footer -->
+      <footer class="mt-4">
+        <p>&copy; 2023 Budget Management App</p>
+      </footer>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    // Add your Vue component logic here
+  };
+  </script>
+  
+  <style scoped>
+  /* Add custom styles specific to this component */
+  </style>
+  
