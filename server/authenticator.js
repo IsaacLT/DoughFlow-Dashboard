4c03f8c6f1da0fdafd70c4083f169067f37327e1
Karl Eriksson <kaeriks@student.chalmers.se>
2023-09-29 13:41:17 +0200

    Fix authenticate file so it can verify tokens sent from the frontend

--- server/authenticator.js
+++ server/authenticator.js
@@ -2,18 +2,29 @@ const jwt = require('jsonwebtoken');
 const config = require('./config');
 
 function authenticateToken(req, res, next) {
-    const token = req.headers.authorization;
-    console.log(token);
-
-    if(!token) {
-        return res.status(401).json({message: 'Unauthorized token', token});
+    const authHeader = req.headers.authorization;
+    
+    //check if header exists
+    if(!authHeader) {
+        return res.status(401).json({message: 'Unauthorized: No Authorization header'});
     }
+
+    const parts = authHeader.split(' ');
+    
+    //check so header is in correct format, and first part of header is "Bearer"
+    if (parts.length !== 2 || parts[0] !== 'Bearer') {
+        return res.status(401).json({message: 'Unauthorized: Invalid Authorization header format'});
+    }
+    
+    //put the actual token for authentication in variable 
+    const token = parts[1];
+    
+    //authenticate token and call next if the token is valid  
     jwt.verify(token, config.jwtKey, (err, user) => {
-      if(err) {
-      return res.status(401).json({message: 'Unauthorized token', token});
-      }
+        if(err) {
+            return res.status(401).json({message: 'Unauthorized token', err});
+        }
         req.user = user;
-        console.log(user);
         next();
     });
 }
